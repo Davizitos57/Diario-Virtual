b39df419dd74de087e908f01c035f188af82f46e
Davi Abner <davi.abner57@gmail.com>
2025-03-03 09:57:42 -0300

    Criação do construtor da classe Diario e das listas de entrada e de usuarios

--- ClassesDiario/Diario.java
+++ ClassesDiario/Diario.java
@@ -1,4 +1,24 @@
 package ClassesDiario;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+//Classe principal do Diario
 public class Diario {
+    //Lista de entradas do diário
+    private List<EntradaDiario> entradas;
+    private List<Usuario> usuarios;
+    //Usuario atualmente conectado
+    private static Usuario usuarioAtual = null;
+
+    //Construtor da classe Diario
+    public Diario(){
+        this.entradas = new ArrayList<>();
+        this.usuarios = new ArrayList<>();
+        //Criacao de uma usuario administrador padrao
+        Usuario admin = new Usuario("adm", "adm", true);
+        usuarios.add(admin);
+    }
 }
