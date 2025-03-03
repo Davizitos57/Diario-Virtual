e9c01a2a548f94759e7c5eafa00660c3b6842e94
Davi Abner <davi.abner57@gmail.com>
2025-03-03 12:48:02 -0300

    Atualização de erros na classe Usuario

--- ClassesDiario/Usuario.java
+++ ClassesDiario/Usuario.java
@@ -6,19 +6,22 @@ public class Usuario {
     private boolean isAdmin;
 
     //Cria um construtor para o Usuario atrelando-o a um username, senha e caso senha ADM verificando a veracidade
-    public Usuario(String username, String senha, boolean isAdmin){
+    public Usuario(String username, String senha, boolean isAdmin) {
         this.username = username;
         this.senha = senha;
         this.isAdmin = isAdmin;
     }
+
     //Retorna o valor booleano de ADM, checando se é verdadeiro ou falso
     public boolean isAdmin() {
         return isAdmin;
     }
+
     //Retorna o valor string de username, comparando se é igual a string escrita pelo Usuario
-    public String getUsername(){
+    public String getUsername() {
         return username;
     }
+
     //Retorna o valor de string de senha, comparando se é igual a string escrita pelo Usuario
     public String getSenha() {
         return senha;
